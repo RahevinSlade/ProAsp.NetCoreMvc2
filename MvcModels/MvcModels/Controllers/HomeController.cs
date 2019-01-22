fff00c67e2f48bae8337f058c3242d21ba0b8452
Rahevin Potter-Clark <rpotter@mapscu.com>
2019-01-22 11:55:03 -0800

    finished set up for ch 26 MvcModels

---
+++ MvcModels/MvcModels/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using MvcModels.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcModels.HomeController
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;
+
+        public HomeController(IRepository repo)
+        {
+            repository = repo;
+        }
+        // GET: /<controller>/
+        public ViewResult Index(int id) => View(repository[id]);
+    }
+}
