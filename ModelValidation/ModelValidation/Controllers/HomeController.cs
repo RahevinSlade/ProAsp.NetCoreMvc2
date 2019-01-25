eea2be24c883d35889fb13be3858789f818ca188
Rahevin Potter-Clark <rpotter@mapscu.com>
2019-01-25 09:41:17 -0800

    finished set up for ch 27

---
+++ ModelValidation/ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ModelValidation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() =>
+            View("MakeBooking", new Appointment { Date = DateTime.Now });
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt) =>
+            View("Completed", appt);
+    }
+}
