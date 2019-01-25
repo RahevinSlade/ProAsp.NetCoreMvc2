c7d64349104c9a5f6a4e45e8aba602db1df096b4
Rahevin Potter-Clark <rpotter@mapscu.com>
2019-01-25 09:56:11 -0800

    refactored code

--- ModelValidation/ModelValidation/Controllers/HomeController.cs
+++ ModelValidation/ModelValidation/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using ModelValidation.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace ModelValidation.Controllers
 {
@@ -10,7 +11,32 @@ namespace ModelValidation.Controllers
             View("MakeBooking", new Appointment { Date = DateTime.Now });
 
         [HttpPost]
-        public ViewResult MakeBooking(Appointment appt) =>
-            View("Completed", appt);
+        public ViewResult MakeBooking(Appointment appt)
+        {
+            if (string.IsNullOrEmpty(appt.ClientName))
+            {
+                ModelState.AddModelError(nameof(appt.ClientName),
+                    "Please enter your name");
+            }
+            if(ModelState.GetValidationState("Date")
+                == ModelValidationState.Valid && DateTime.Now > appt.Date)
+            {
+                ModelState.AddModelError(nameof(appt.Date),
+                    "Please enter a date in the future");
+            }
+            if (!appt.TermsAccepted)
+            {
+                ModelState.AddModelError(nameof(appt.TermsAccepted),
+                    "You must accept the terms");
+            }
+
+            if (ModelState.IsValid)
+            {
+                return View("Completed", appt);
+            } else
+            {
+                return View();
+            }
+        }
     }
 }
