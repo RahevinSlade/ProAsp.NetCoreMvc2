9eb2199dc58f3afa20d13effb0704ab6fa4c2d16
Rahevin Potter-Clark <rpotter@mapscu.com>
2019-01-24 16:55:19 -0800

    finished ch 26, add name body script

--- MvcModels/MvcModels/Controllers/HomeController.cs
+++ MvcModels/MvcModels/Controllers/HomeController.cs
@@ -30,21 +30,9 @@ namespace MvcModels.HomeController
         //public string Header([FromHeader(Name = "Accept-Language")] string accept) => $"Header: {accept}";
         public ViewResult Header(HeaderModel model) => View(model);
 
-        public ViewResult Create() => View(new Person());
+        public ViewResult Body() => View();
 
         [HttpPost]
-        public ViewResult Create(Person model) => View("Index", model);
-
-        public ViewResult DisplaySummary(
-            [Bind(Prefix = nameof(Person.HomeAddress))] AddressSummary summary) 
-                => View(summary);
-
-        //public ViewResult Names(string[] names) => View(names ?? new string[0]);
-
-        public ViewResult Names(IList<string> names) =>
-            View(names ?? new List<string>());
-
-        public ViewResult Address(IList<AddressSummary> addresses) =>
-            View(addresses ?? new List<AddressSummary>());
+        public Person Body([FromBody]Person model) => model;
     }
 }
